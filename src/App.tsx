e9f854eb8c7987157fa0e5f2101d6a6badcf789f
Connor <connorgawley@yahoo.com>
2023-09-13 11:59:10 -0700

    init vite app

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, this is our project</h1>
+      <ul>
+        <li>Kanta Onishi</li>
+        <li>Dexter Norris</li>
+        <li>Connor Gawley</li>
+        <li>Sarah Suleman</li>
+        <li>Emma Martin</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
